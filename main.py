ad51bce450c2b7980be1ecc4cc1f42dd37b6a8fd
Suvamp <patelsuvam621@gmail.com>
2023-07-07 16:57:08 -0700

    Add description on the new columns

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Suvam Patel")
+    content = """
+    Hello there. I am Suvam. This is where I keep all my python projects. Feel free to look around.
+    """
+    st.info(content)
+
+content2 = """
+Here are my apps. Feel free to contact me. Here is my github: https://github.com/Suvamp
+"""
+st.write(content2)
