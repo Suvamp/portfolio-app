de0a6e18c81d9805e2ced0d4d02f7f2ff429a44a
Suvamp <patelsuvam621@gmail.com>
2023-07-12 15:01:47 -0700

    Added github links for productivity and portfolio app

--- send_email.py
+++ send_email.py
@@ -1,28 +1,20 @@
 import smtplib, ssl
-
-host = "smtp.gmail.com"
-port = 465
-
-user_email = "suvamp@uci.edu"
-password = "dltoscijeurxzehw"
-
-receiver = "patelsuvam621@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hello!
-Hello.
-
-I am away at the moment. Someone from our team should reach out to you ASAP. 
+import os
 
 
-Best Regards,
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    user_email = "suvamp@uci.edu"
+    password = "dltoscijeurxzehw"
+
+    receiver = "patelsuvam621@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_email, password)
+        server.sendmail(user_email, receiver, message)
 
 
-Suvam Patel
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(user_email, password)
-    server.sendmail(user_email, receiver, message)
 
